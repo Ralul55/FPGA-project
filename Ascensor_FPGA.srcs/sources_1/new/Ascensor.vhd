f4da6700f7278a3dfe1a3ac9bd0a7fd22610e2bf
Ralul55 <130755244+Ralul55@users.noreply.github.com>
2025-12-21 17:37:37 +0100

    Creación del decodificador y forma general
    He programado el decodificador que selecciona el piso deseado en función del botón  que se pulse, implementando una lógica que hace que los botones pulsados antes de que se alcance el piso deseado sean ignorados.
    
    También le he dado la forma general a la máquina de estados siguiendo el modelo de la máquina que programamos en el laboratorio como una plantilla fácil de seguir.

--- Ascensor_FPGA.srcs/sources_1/new/Ascensor.vhd
+++ Ascensor_FPGA.srcs/sources_1/new/Ascensor.vhd
@@ -22,17 +22,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- Esta será la entidad TOP
 
 entity Ascensor is
---  Port ( );
+    Port ( 
+        boton_i: IN std_logic_vector(4 downto 1); -- El rango raro de esto es debido a que los botones exteriores del fichero de ctes cuentan 
+        boton_e: IN std_logic_vector(4 downto 1)  -- desde el 1 (mirar fichero de ctes)
+        
+    );
 end Ascensor;
 
 architecture Structural of Ascensor is
